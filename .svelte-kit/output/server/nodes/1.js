1d6ad9eb59d96f8a6de9fc812535773bb0060e23
Hesham Alatrash <heshamoov90@gmail.com>
2025-08-15 03:15:42 +0400

    Add .gitignore and remove unnecessary files

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DWSeW63s.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/Bykrahq8.js","_app/immutable/chunks/C1cF_f_f.js","_app/immutable/chunks/C9t7Hmsj.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/4.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 4;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/forms/_id_/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.DdSBFU37.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/Bykrahq8.js","_app/immutable/chunks/C1cF_f_f.js","_app/immutable/chunks/oCXtbbpy.js","_app/immutable/chunks/sx0AX0zF.js","_app/immutable/chunks/CGMAZzXY.js","_app/immutable/chunks/Bfc47y5P.js","_app/immutable/chunks/BhgOVFHs.js","_app/immutable/chunks/C9t7Hmsj.js","_app/immutable/chunks/D1b5UXiN.js"];
-export const stylesheets = [];
-export const fonts = [];
